34a913a5eb2911471b076d4eb6e54472d2ab4eae
Isaacdlcrz <62781353+Isaacdlcrz@users.noreply.github.com>
2024-09-02 08:50:19 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -204,7 +204,7 @@ fig1, ax1 = plt.subplots()
 
 #Generación del gráfico
 sns.set(style = "darkgrid")
-sns.histplot(data = datos_df[histo_selected])
+sns.histplot(data = datos_ganancias_df)
 ax1.set_title('Histograma de Valores')
 ax1.set_xlabel(histo_selected)
 ax1.set_ylabel('Frecuencia')
